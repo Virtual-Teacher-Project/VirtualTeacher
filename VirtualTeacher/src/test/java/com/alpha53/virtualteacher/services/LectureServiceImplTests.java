abfee6ff13f56c175f051f3e7afb97c9157819b8
Borislav Chavdarov <52974503+BorislavChavdarov@users.noreply.github.com>
2023-12-14 16:49:52 -0800

    about us and contacts page

--- VirtualTeacher/src/test/java/com/alpha53/virtualteacher/services/LectureServiceImplTests.java
+++ VirtualTeacher/src/test/java/com/alpha53/virtualteacher/services/LectureServiceImplTests.java
@@ -1,5 +1,7 @@
 package com.alpha53.virtualteacher.services;
 
+import com.alpha53.virtualteacher.exceptions.EntityDuplicateException;
+import com.alpha53.virtualteacher.exceptions.EntityNotFoundException;
 import com.alpha53.virtualteacher.models.Course;
 import com.alpha53.virtualteacher.models.Lecture;
 import com.alpha53.virtualteacher.models.User;
@@ -14,6 +16,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 public class LectureServiceImplTests {
@@ -38,10 +43,48 @@ public class LectureServiceImplTests {
         this.storageService = storageService;
         this.lectureService = lectureService;
     }
+//    Lecture get(int courseId, int lectureId, User user);
+//
+//    List<Lecture> getAllByCourseId(int courseId, User user);
+//
+//    void create(Lecture lecture, User user, MultipartFile assignment);
+//
+//
+//    void update(Lecture lecture, User user, MultipartFile assignment);
+//
+//    void delete(int courseId, int lectureId, User user);
+//
+//    void uploadSolution(int courseId, int lectureId, User user, MultipartFile assignmentSolution);
+//
+//    boolean isAssignmentExist(int lectureId);
+
+    @Test
+    public void get_Should_Call_IfValid() {
+        User mockUser = Helpers.createMockAdmin();
+        Course mockCourse = Helpers.createMockCourse();
+        Lecture mockLecture = Helpers.createMockLecture();
 
 
-    public void get_Should_ReturnLecture_When_UserIsCreator(){
+        Mockito.when(lectureDao.get(Mockito.anyInt())).thenReturn(mockLecture);
 
+        lectureService.get(mockCourse.getCourseId(),mockLecture.getId(),mockUser);
+
+
+        Mockito.verify(lectureDao, Mockito.times(1)).get(Mockito.anyInt());
+    }
+
+
+
+    @Test
+    public void get_Should_ThrowWhenUser_NotEnrolled() {
+//        User mockUser = Helpers.createMockTeacher();
+//        Course mockCourse = Helpers.createMockCourse();
+//
+//        mockUser.setRole(Helpers.createMockTeacherRole());
+//
+//        Mockito.when(courseDao.getByTitle(Mockito.anyString())).thenReturn(mockCourse);
+//
+//        Assertions.assertThrows(EntityDuplicateException.class, () -> courseService.create(mockCourse, mockUser));
     }
 
 
