ea5c1a98f98e6bdb91d131fbdc574ed6f6279b7d
VikMit <viktorivmitrev@gmail.com>
2024-01-03 17:58:09 +0200

    adding ToDoList.

---
+++ VirtualTeacher/src/main/java/com/alpha53/virtualteacher/ToDoList.java
@@ -0,0 +1,14 @@
+package com.alpha53.virtualteacher;
+
+public class ToDoList {
+    // TODO: 3.01.24 Clear unused CSS, HTML, JS and theme files;
+    // TODO: 3.01.24 Refactor Course section; 
+    // TODO: 3.01.24 Add tests for lecture and other unfinished services;
+    // TODO: 3.01.24 Try writing some integration tests;
+    // TODO: 3.01.24 Research and use Docker;
+    // TODO: 3.01.24 Add caching functionality for user roles - research redis;
+    // TODO: 3.01.24 Implement CI/CD;
+    // TODO: 3.01.24 Host the project online (e.g. using AWS);
+
+
+}
